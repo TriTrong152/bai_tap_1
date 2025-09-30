78d14c7dc0615229f2ef336c176ac03aee640805
TriTrong152 <tritrongnguyen152@gmail.com>
2025-09-30 23:25:22 +0700

    Delete Bai3/bai3/myproject directory

--- Bai3/bai3/myproject/actuators.h
+++
@@ -1,8 +0,0 @@
-#ifndef ACTUATORS_H
-#define ACTUATORS_H
-
-
-#include "config.h"
-extern void pump_mode(watering_state_t mode);
-extern void run_the_pump(watering_logic_t *logic);
-#endif
--- Bai3/bai3/myproject/sensor.h
+++
@@ -1,9 +0,0 @@
-#ifndef SENSORS_H
-#define SENSORS_H
-
-#include "config.h"
-
-void sensors_init(void);
-void sensors_read(SensorData *data);
-
-#endif
